3089e662ca4f872013bc6b46c902a4098a7b1497
davidrip83 <d.a.riphagen@gmail.com>
2015-06-19 23:56:08 -0700

    Adding new changes with more comments and description

--- cachematrix.R
+++ cachematrix.R
@@ -10,13 +10,14 @@
 makeCacheMatrix <- function(x = matrix()) { ## input is a matrix x
     i <- NULL ## i is the special matrix object
     set <- function(y) { ## assign values to x and i
-        x <<- y
-        i <<- NULL
+        x <<- y ## substitute x with main input y from function above
+        i <<- NULL ## old value of inverse not required anymore
     }
     get <- function() x 
-    setinverse <- function(solve) i <<- inverse ## calculate inverse
+    ## note: setinverse() and getinverse() don't calculate the inverse
+    setinverse <- function(solve) i <<- inverse ## set inverse
     getinverse <- function() i ## get inverse
-    list(set = set, get = get, ## list to set and get inverse
+    list(set = set, get = get, ## store function in makeCacheMatrix
          setinverse = setinverse, 
          getinverse = getinverse) 
 }
@@ -26,13 +27,13 @@ makeCacheMatrix <- function(x = matrix()) { ## input is a matrix x
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
-    i <- x$getinverse() ## get the inverse
-    if(!is.null(i)) { ## if the inverse doesn't exist get the cached inverse
+    i <- x$getinverse() ## get the inverse from makeCacheMatrix and store in i
+    if(!is.null(i)) { ## if the inverse is stored get the cached inverse
         message("getting cached data")
         return(i) ## return the inverse from the cache
     }
-    data <- x$get() 
+    data <- x$get() ## if the inverse is not stored in cache
     i <- solve(data, ...) ## calculate the inverse of data
     x$setinverse(i) ## set the inverse equal to i
-    i
+    i ## return i
 }
